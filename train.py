c4e1dda90eb0e9045e2b53189d2f2a8063c40afd
yjx <145849455+kamuyix@users.noreply.github.com>
2024-08-11 18:49:33 +0800

    Add files via upload

--- train.py
+++ train.py
@@ -35,8 +35,8 @@ parser.add_argument('--imu_dropout', type=float, default=0, help='dropout for th
 parser.add_argument('--head_size_a', type=int, default=64, help='')
 
 parser.add_argument('--rwkv_out_size', type=int, default=512)
-parser.add_argument('--rnn_dropout_out', type=float, default=0.2, help='dropout for the LSTM output layer')
-parser.add_argument('--rnn_dropout_between', type=float, default=0.2, help='dropout within LSTM')
+parser.add_argument('--rnn_dropout_out', type=float, default=0.2, help='dropout for the rwkv output layer')
+parser.add_argument('--rnn_dropout_between', type=float, default=0.2, help='dropout within rwkv')
 parser.add_argument('--weight_decay', type=float, default=5e-6, help='weight decay for the optimizer')
 parser.add_argument('--batch_size', type=int, default=16, help='batch size')
 parser.add_argument('--seq_len', type=int, default=16, help='sequence length for LSTM')
