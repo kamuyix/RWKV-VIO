fe328b703a0a80437c812ee55b8b47791dc0e59f
yjx <145849455+kamuyix@users.noreply.github.com>
2024-08-11 17:54:00 +0800

    Add files via upload

--- train.py
+++ train.py
@@ -1,16 +1,22 @@
 import argparse
 import os
+import warnings
+
 import torch
 import logging
 from path import Path
-from utils import custom_transform
-from vio_dataset.KITTI_dataset import KITTI
+from vio_utils import custom_transform
+from dataset_process.KITTI_dataset import KITTI
 from model import DeepVIO
 from collections import defaultdict
-from utils.kitti_eval import KITTI_tester
+from vio_utils.kitti_eval import KITTI_tester
 import numpy as np
 import math
-
+import matplotlib.pyplot as plt
+import torch.optim as optim
+import torch.optim.lr_scheduler as lr_scheduler
+from torch.optim.lr_scheduler import CosineAnnealingLR
+# 设置参数
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('--data_dir', type=str, default='/mnt/data2/Mutil_data/Visual-Selective-VIO-main/data', help='path to the dataset')
 parser.add_argument('--gpu_ids', type=str, default='0', help='gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU')
@@ -26,107 +32,94 @@ parser.add_argument('--v_f_len', type=int, default=512, help='visual feature len
 parser.add_argument('--i_f_len', type=int, default=256, help='imu feature length')
 parser.add_argument('--fuse_method', type=str, default='cat', help='fusion method [cat, soft, hard]')
 parser.add_argument('--imu_dropout', type=float, default=0, help='dropout for the IMU encoder')
+parser.add_argument('--head_size_a', type=int, default=64, help='')
 
-parser.add_argument('--rnn_hidden_size', type=int, default=1024, help='size of the LSTM latent')
+parser.add_argument('--rnn_hidden_size', type=int, default=512, help='size of the LSTM latent')
 parser.add_argument('--rnn_dropout_out', type=float, default=0.2, help='dropout for the LSTM output layer')
 parser.add_argument('--rnn_dropout_between', type=float, default=0.2, help='dropout within LSTM')
-
 parser.add_argument('--weight_decay', type=float, default=5e-6, help='weight decay for the optimizer')
 parser.add_argument('--batch_size', type=int, default=16, help='batch size')
-parser.add_argument('--seq_len', type=int, default=11, help='sequence length for LSTM')
-parser.add_argument('--workers', type=int, default=4, help='number of workers')
+parser.add_argument('--seq_len', type=int, default=16, help='sequence length for LSTM')
+parser.add_argument('--workers', type=int, default=32, help='number of workers')
 parser.add_argument('--epochs_warmup', type=int, default=40, help='number of epochs for warmup')
-parser.add_argument('--epochs_joint', type=int, default=40, help='number of epochs for joint training')
 parser.add_argument('--epochs_fine', type=int, default=20, help='number of epochs for finetuning')
-parser.add_argument('--lr_warmup', type=float, default=5e-4, help='learning rate for warming up stage')
-parser.add_argument('--lr_joint', type=float, default=5e-5, help='learning rate for joint training stage')
+parser.add_argument('--lr_warmup', type=float, default=1e-4, help='learning rate for warming up stage')
 parser.add_argument('--lr_fine', type=float, default=1e-6, help='learning rate for finetuning stage')
-parser.add_argument('--eta', type=float, default=0.05, help='exponential decay factor for temperature')
-parser.add_argument('--temp_init', type=float, default=5, help='initial temperature for gumbel-softmax')
-parser.add_argument('--Lambda', type=float, default=3e-5, help='penalty factor for the visual encoder usage')
+
+parser.add_argument('--n_layer', type=int, default=4, help='num of rwkv')
+parser.add_argument('--n_embd', type=int, default=768, help='v_f + i_f')
+parser.add_argument('--dim_att', type=int, default=768, help='')
+parser.add_argument('--dim_ffn', type=int, default=2048, help='')
+parser.add_argument("--head_size_divisor", default=12, type=int)
+parser.add_argument("--dropout", default=0.05, type=float)
+parser.add_argument("--grad_cp", default=0, type=int)
 
 parser.add_argument('--experiment_name', type=str, default='experiment', help='experiment name')
 parser.add_argument('--optimizer', type=str, default='Adam', help='type of optimizer [Adam, SGD]')
 
 parser.add_argument('--pretrain_flownet',type=str, default='./pretrain_models/flownets_bn_EPE2.459.pth.tar', help='wehther to use the pre-trained flownet')
 parser.add_argument('--pretrain', type=str, default=None, help='path to the pretrained model')
-parser.add_argument('--hflip', default=False, action='store_true', help='whether to use horizonal flipping as augmentation')
-parser.add_argument('--color', default=False, action='store_true', help='whether to use color augmentations')
+parser.add_argument('--hflip', default=False, action='store_true', help='whether to use horizontal flip data augmentation')
+parser.add_argument('--color', default=False, action='store_true', help='whether to use color data augmentation')
 
-parser.add_argument('--print_frequency', type=int, default=10, help='print frequency for loss values')
-parser.add_argument('--weighted', default=False, action='store_true', help='whether to use weighted sum')
+parser.add_argument('--print_frequency', type=int, default=10, help='frequency of printing loss')
+parser.add_argument('--weighted', default=False, action='store_true', help='whether to use weighted loss')
 
 args = parser.parse_args()
 
-# Set the random seed
+# 设置随机种子
 torch.manual_seed(args.seed)
 np.random.seed(args.seed)
 
-def update_status(ep, args, model):
-    if ep < args.epochs_warmup:  # Warmup stage
-        lr = args.lr_warmup
-        selection = 'random'
-        temp = args.temp_init
-        for param in model.module.Policy_net.parameters(): # Disable the policy network
-            param.requires_grad = False
-    elif ep >= args.epochs_warmup and ep < args.epochs_warmup + args.epochs_joint: # Joint training stage
-        lr = args.lr_joint
-        selection = 'gumbel-softmax'
-        temp = args.temp_init * math.exp(-args.eta * (ep-args.epochs_warmup))
-        for param in model.module.Policy_net.parameters(): # Enable the policy network
-            param.requires_grad = True
-    elif ep >= args.epochs_warmup + args.epochs_joint: # Finetuning stage
-        lr = args.lr_fine
-        selection = 'gumbel-softmax'
-        temp = args.temp_init * math.exp(-args.eta * (ep-args.epochs_warmup))
-    return lr, selection, temp
-
-def train(model, optimizer, train_loader, selection, temp, logger, ep, p=0.5, weighted=False):
-    
+def train(model, optimizer, train_loader, logger, ep, weighted=False):
     mse_losses = []
-    penalties = []
     data_len = len(train_loader)
 
     for i, (imgs, imus, gts, rot, weight) in enumerate(train_loader):
-
-        imgs = imgs.cuda().float()
-        imus = imus.cuda().float()
-        gts = gts.cuda().float() 
-        weight = weight.cuda().float()
+        imgs = imgs.to('cuda', dtype=torch.bfloat16)
+        imus = imus.to('cuda', dtype=torch.bfloat16)
+        gts = gts.to('cuda', dtype=torch.bfloat16)
+        weight = weight.to('cuda', dtype=torch.bfloat16)
 
         optimizer.zero_grad()
-                
-        poses, decisions, probs, _ = model(imgs, imus, is_first=True, hc=None, temp=temp, selection=selection, p=p)
-        
+
+        poses = model(imgs, imus)
+
         if not weighted:
             angle_loss = torch.nn.functional.mse_loss(poses[:,:,:3], gts[:, :, :3])
             translation_loss = torch.nn.functional.mse_loss(poses[:,:,3:], gts[:, :, 3:])
         else:
-            weight = weight/weight.sum()
+            weight = weight / weight.sum()
             angle_loss = (weight.unsqueeze(-1).unsqueeze(-1) * (poses[:,:,:3] - gts[:, :, :3]) ** 2).mean()
             translation_loss = (weight.unsqueeze(-1).unsqueeze(-1) * (poses[:,:,3:] - gts[:, :, 3:]) ** 2).mean()
-        
-        pose_loss = 100 * angle_loss + translation_loss        
-        penalty = (decisions[:,:,0].float()).sum(-1).mean()
-        loss = pose_loss + args.Lambda * penalty 
-        
-        loss.backward()
+
+        pose_loss = 100 * angle_loss + translation_loss
+
+        pose_loss.backward()
         optimizer.step()
-        
-        if i % args.print_frequency == 0: 
-            message = f'Epoch: {ep}, iters: {i}/{data_len}, pose loss: {pose_loss.item():.6f}, penalty: {penalty.item():.6f}, loss: {loss.item():.6f}'
+
+        if i % args.print_frequency == 0:
+            message = f"Epoch: {ep}, iters: {i}/{data_len}, pose loss: {pose_loss.item():.6f}"
             print(message)
             logger.info(message)
 
         mse_losses.append(pose_loss.item())
-        penalties.append(penalty.item())
 
-    return np.mean(mse_losses), np.mean(penalties)
+    return mse_losses
 
+def plot_losses(all_iters, pose_losses):
+    plt.figure(figsize=(10, 5))
+    plt.plot(all_iters, pose_losses, label='Pose Loss')
+    plt.xlabel('Iteration')
+    plt.ylabel('Loss')
+    plt.title('Training Losses')
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+    plt.savefig('training_losses.png')
 
 def main():
-
-    # Create Dir
+    # 创建目录
     experiment_dir = Path('./results')
     experiment_dir.mkdir_p()
     file_dir = experiment_dir.joinpath('{}/'.format(args.experiment_name))
@@ -135,20 +128,21 @@ def main():
     checkpoints_dir.mkdir_p()
     log_dir = file_dir.joinpath('logs/')
     log_dir.mkdir_p()
-    
-    # Create logs
+
+    # 创建日志
     logger = logging.getLogger(args.experiment_name)
     logger.setLevel(logging.INFO)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    file_handler = logging.FileHandler(str(log_dir) + '/train_%s.txt'%args.experiment_name)
+    file_handler = logging.FileHandler(str(log_dir) + '/train_%s.txt' % args.experiment_name)
     file_handler.setLevel(logging.INFO)
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
     logger.info('----------------------------------------TRAINING----------------------------------')
     logger.info('PARAMETER ...')
     logger.info(args)
-    
-    # Load the dataset
+
+    warnings.filterwarnings("ignore", category=UserWarning, module="torchvision")
+
     transform_train = [custom_transform.ToTensor(),
                        custom_transform.Resize((args.img_h, args.img_w))]
     if args.hflip:
@@ -163,7 +157,7 @@ def main():
                         transform=transform_train
                         )
     logger.info('train_dataset: ' + str(train_dataset))
-    
+
     train_loader = torch.utils.data.DataLoader(
         train_dataset,
         batch_size=args.batch_size,
@@ -171,8 +165,8 @@ def main():
         num_workers=args.workers,
         pin_memory=True
     )
-    
-    # GPU selections
+
+    # GPU 选择
     str_ids = args.gpu_ids.split(',')
     gpu_ids = []
     for str_id in str_ids:
@@ -181,23 +175,23 @@ def main():
             gpu_ids.append(id)
     if len(gpu_ids) > 0:
         torch.cuda.set_device(gpu_ids[0])
-    
-    # Initialize the tester
+
+    # 初始化测试器
     tester = KITTI_tester(args)
 
-    # Model initialization
+    # 模型初始化
     model = DeepVIO(args)
 
-    # Continual training or not
+    # 是否加载预训练模型
     if args.pretrain is not None:
         model.load_state_dict(torch.load(args.pretrain))
-        print('load model %s'%args.pretrain)
-        logger.info('load model %s'%args.pretrain)
+        print('Loaded pre-trained model %s' % args.pretrain)
+        logger.info('Loaded pre-trained model %s' % args.pretrain)
     else:
         print('Training from scratch')
         logger.info('Training from scratch')
-    
-    # Use the pre-trained flownet or not
+
+    # 是否使用预训练的 FlowNet
     if args.pretrain_flownet and args.pretrain is None:
         pretrained_w = torch.load(args.pretrain_flownet, map_location='cpu')
         model_dict = model.Feature_net.state_dict()
@@ -205,68 +199,79 @@ def main():
         model_dict.update(update_dict)
         model.Feature_net.load_state_dict(model_dict)
 
-    # Feed model to GPU
+    # 模型加载到 GPU
     model.cuda(gpu_ids[0])
-    model = torch.nn.DataParallel(model, device_ids = gpu_ids)
 
-    pretrain = args.pretrain 
-    init_epoch = int(pretrain[-7:-4])+1 if args.pretrain is not None else 0    
-    
-    # Initialize the optimizer
-    if args.optimizer == 'SGD':
-        optimizer = torch.optim.SGD(model.parameters(), lr=1e-4, momentum=0.9)
-    elif args.optimizer == 'Adam':
-        optimizer = torch.optim.Adam(model.parameters(), lr=1e-4, betas=(0.9, 0.999), 
-                                     eps=1e-08, weight_decay=args.weight_decay)
-    
-    best = 10000
+    # 初始化优化器
+    optimizer = optim.Adam(model.parameters(), lr=args.lr_warmup, betas=(0.9, 0.999), eps=1e-08, weight_decay=args.weight_decay)
+    total_epochs = args.epochs_warmup + args.epochs_fine
 
-    for ep in range(init_epoch, args.epochs_warmup+args.epochs_joint+args.epochs_fine):
-        
-        lr, selection, temp = update_status(ep, args, model)
-        optimizer.param_groups[0]['lr'] = lr
-        message = f'Epoch: {ep}, lr: {lr}, selection: {selection}, temperaure: {temp:.5f}'
+    # 定义学习率调度器
+    scheduler = CosineAnnealingLR(optimizer, T_max=args.epochs_warmup)
+
+    best = float('inf')
+    all_iters = []
+    pose_losses = []
+    iter_count = 0
+
+
+    for ep in range(total_epochs):
+
+        # 动态调整学习率
+        if ep < args.epochs_warmup:
+            scheduler.step()
+        else:
+            for param_group in optimizer.param_groups:
+                param_group['lr'] = args.lr_fine
+
+        message = f'Epoch: {ep}, lr: {optimizer.param_groups[0]["lr"]:.8f}'
         print(message)
         logger.info(message)
 
         model.train()
-        avg_pose_loss, avg_penalty_loss = train(model, optimizer, train_loader, selection, temp, logger, ep, p=0.5)
-        
-        # Save the model after training
-        torch.save(model.module.state_dict(), f'{checkpoints_dir}/{ep:003}.pth')
-        message = f'Epoch {ep} training finished, pose loss: {avg_pose_loss:.6f}, penalty_loss: {avg_penalty_loss:.6f}, model saved'
-        print(message)
-        logger.info(message)
-        
-        if ep > args.epochs_warmup+args.epochs_joint:
-            # Evaluate the model
+        mse_losses = train(model, optimizer, train_loader, logger, ep)
+
+        for mse_loss in mse_losses:
+            iter_count += 1
+            all_iters.append(iter_count)
+            pose_losses.append(mse_loss)
+
+        # 每10个 epoch 保存一次模型
+        if (ep + 1) % 10 == 0:
+            torch.save(model.state_dict(), f'{checkpoints_dir}/{ep+1:003}.pth')
+            message = f'Checkpoint saved for epoch {ep+1}'
+            print(message)
+            logger.info(message)
+
+        # 评估模型
+        if ep >= args.epochs_warmup:
             print('Evaluating the model')
             logger.info('Evaluating the model')
-            with torch.no_grad(): 
+            with torch.no_grad():
                 model.eval()
-                errors = tester.eval(model, selection='gumbel-softmax', num_gpu=len(gpu_ids))
-        
+                errors = tester.eval(model, num_gpu=len(gpu_ids))
+
             t_rel = np.mean([errors[i]['t_rel'] for i in range(len(errors))])
             r_rel = np.mean([errors[i]['r_rel'] for i in range(len(errors))])
             t_rmse = np.mean([errors[i]['t_rmse'] for i in range(len(errors))])
             r_rmse = np.mean([errors[i]['r_rmse'] for i in range(len(errors))])
-            usage = np.mean([errors[i]['usage'] for i in range(len(errors))])
 
             if t_rel < best:
-                best = t_rel 
-                torch.save(model.module.state_dict(), f'{checkpoints_dir}/best_{best:.2f}.pth')
-        
-            message = f'Epoch {ep} evaluation finished , t_rel: {t_rel:.4f}, r_rel: {r_rel:.4f}, t_rmse: {t_rmse:.4f}, r_rmse: {r_rmse:.4f}, usage: {usage:.4f}, best t_rel: {best:.4f}'
+                best = t_rel
+                torch.save(model.state_dict(), f'{checkpoints_dir}/best_{best:.2f}.pth')
+
+            message = f'Epoch {ep+1} evaluation finished, t_rel: {t_rel:.4f}, r_rel: {r_rel:.4f}, t_rmse: {t_rmse:.4f}, r_rmse: {r_rmse:.4f}, best t_rel: {best:.4f}'
             logger.info(message)
             print(message)
-    
+
+    # 训练结束后保存损失图
+    plot_losses(all_iters, pose_losses)
+
+    # 保存最终模型
+    torch.save(model.state_dict(), f'{checkpoints_dir}/final_model.pth')
     message = f'Training finished, best t_rel: {best:.4f}'
     logger.info(message)
     print(message)
 
 if __name__ == "__main__":
     main()
-
-
-
-
